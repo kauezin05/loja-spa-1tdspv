43c07790ff6a3f5a72103f28d84c88411a85c4c3
Kaue <kauezinho_naruto@hotmail.com>
2023-10-26 23:41:40 -0300

    Get consumindo API local com arquivo jsx

--- src/app/produtos/fazenda/loja/laticinios/[id]/page.jsx
+++ src/app/produtos/fazenda/loja/laticinios/[id]/page.jsx
@@ -1,45 +1,33 @@
-import { redirect } from "next/dist/server/api-utils";
 import Image from "next/image";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function Item({params}) {
-
-  let queijo;
-  try{
-    const response = await fetch(`http://localhost:3000/api/laticinios/${params.id}`);
-    queijo = await response.json();
-  }catch(error){
-    console.log(error);
-    redirect("/error");
-  }
-  
-
+        let queijo;
+        try{
+            const response = await fetch(`http://localhost:3000/api/laticinios/${params.id}`);
+            queijo = await response.json();
+        }catch(error){
+            console.log(error);
+            redirect("/error");
+        }
   return (
     <div>
-        <h1>ITEM</h1>
-        <section>
-    
-        
-          
-            <div key={queijo.id}>
-                <h1>PRODUTO: {queijo.nome}</h1>
-                <h2>{queijo.categoria}</h2>
-                <figure>
-                    <Image
-                    src={queijo.imagem}
-                    width={100}
-                    height={100}
-                    alt={queijo.descricao}
-                    />
-                    <figcaption> {queijo.descricao}  R$-<span>{queijo.preco}</span></figcaption>
-                </figure>
 
-                
+        <h1>ITEMS</h1>
 
-            </div>
-          
-        </section>
-      
+        <section>  
+                    {
+                    <div key={queijo.id}>
+                        <h1>PRODUTO: {queijo.nome}</h1>
+                        <h2>CATEGORIA: {queijo.categoria}</h2>
+                        <figure>
+                            <Image src={queijo.imagem} width={100} height={100} alt={queijo.descricao} />
+                            <figcaption>{queijo.descricao} - R$ <span>{queijo.preco}</span></figcaption>
+                        </figure>
+                    </div>
+                }
+           </section>
+
     </div>
-  );
+  )
 }
--- src/app/produtos/fazenda/loja/laticinios/page.jsx
+++ src/app/produtos/fazenda/loja/laticinios/page.jsx
@@ -1,73 +1,54 @@
-import { redirect } from "next/dist/server/api-utils";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function TodosLaticinios() {
-
-  let queijos;
-  try{
-    const response = await fetch("http://localhost:3000/api/laticinios");
-    queijos = await response.json();
-  }catch(error){
-    console.log(error);
-    redirect("/error");
-  }
-  
-
+        let queijos;
+        try{
+            const response = await fetch("http://localhost:3000/api/laticinios/0");
+            queijos = await response.json();
+        }catch(error){
+            console.log(error);
+            redirect("/error");
+        }
   return (
-    <div>
-      <h1>Laticínios</h1>
+    <div className="lat-prod-all">
 
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
+        <h1>Laticínios</h1>
 
-            <th>Imagem</th>
+        <table>
+            <thead>
+                <tr>    
+                    <th>ID</th>
+                    <th>Imagem</th>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th>Categoria</th>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                {queijos.map((queijo) => (
+                    <tr key={queijo.id}>
+                        <td>{queijo.id}</td>
+                        <td>
+                            <Link href={`/produtos/fazenda/loja/laticinios/${queijo.id}`}>
+                            <Image src={queijo.imagem} width={100} height={100} alt={queijo.descricao} /></Link>
+                        </td>
+                        <td>{queijo.nome}</td>
+                        <td>{queijo.preco}</td>
+                        <td>{queijo.categoria}</td>
+                        <td>{queijo.descricao}</td>
+                    </tr>
+                ))}
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colSpan="6">Total de Laticínios: {queijos.length}</td>
+                </tr>
+            </tfoot>
+        </table>
 
-            <th>Nome</th>
-
-            <th>Preço</th>
-
-            <th>Categoria</th>
-
-            <th>Descrição</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {queijos.map((queijo) => (
-            <tr key={queijo.id}>
-              <td>{queijo.id}</td>
-
-              <td>
-                <Link href={`http://localhost:3000/api/laticinios/${params.id}`}>
-                <Image
-                  src={queijo.imagem}
-                  width={100}
-                  height={100}
-                  alt={queijo.descricao}
-                /></Link>
-              </td>
-
-              <td>{queijo.nome}</td>
-
-              <td>{queijo.preco}</td>
-
-              <td>{queijo.categoria}</td>
-
-              <td>{queijo.descricao}</td>
-            </tr>
-          ))}
-        </tbody>
-
-        <tfoot>
-          <tr>
-            <td colSpan="6">Total de Laticínios: {queijos.length}</td>
-          </tr>
-        </tfoot>
-      </table>
     </div>
-  );
+  )
 }
-
